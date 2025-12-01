f48d198e0887eca6d24e42487940593821e9f3e0
Matin.Am <matin.amani101013@gmail.com>
2025-12-01 18:32:49 +0330

    improved caching and made some changes

--- home/views.py
+++ home/views.py
@@ -1,6 +1,5 @@
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView,DetailView
-from django.utils.http import urlencode
 from django.views.decorators.cache import cache_page
 from django.utils.decorators import method_decorator
 from .models import Product,Category
